e48822538069d3cb1b93300aab4575dd09bf9ad0
Guilherme Soares <guilherme.soares@deel.com>
2024-08-26 08:12:05 -0300

    Add list people sample

---
+++ samples/list-people/README.md
@@ -0,0 +1,13 @@
+# Show People Data sample
+
+Simple Create React App as client side app and Node with Express as a backend to demonstrate how to interact with Deel's API.
+
+## How to run the project locally
+
+1. Generate a new token following [this tutorial](https://developer.deel.com/docs/api-tokens-1).
+2. Update `API_TOKEN` value with generated token in `.env`file inside `server` folder.
+3. Run `cd samples/list-people/` from root folder
+4. Run `npm install`
+5. Run `npm run setup`
+6. Run `npm start`
+
---
+++ samples/list-people/server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const axios = require('axios').default;
+const cors = require('cors');
+const app = express();
+
+app.use(express.json())
+app.use(cors());
+
+const getHeaders = () => ({
+    'Authorization': `Bearer ${process.env.API_TOKEN}`,
+    'Content-Type': 'application/json'
+});
+app.get('/api/people', async (_, res) => {
+    const response = await axios.get(`${process.env.API_HOST}/rest/v2/people`, {
+        headers: getHeaders()
+    });
+    res.json(response.data);
+});
+
+app.get('/api/people/:id', async (req, res) => {
+    const response = await axios.get(`${process.env.API_HOST}/rest/v2/people/${req.params.id}`, {
+        headers: getHeaders()
+    });
+    res.json(response.data);
+});
+
+const port = 3099;
+app.listen(3099,() => {
+    console.log(`Server is listening on port ${3099}`)
+});
